c1b1fdbacb17194beb3c5a720f13f94a25f6d3f8
Shahar Shitrit <shahars86@gmail.com>
2024-11-13 12:09:06 +0200

    change provider version

--- terraform/aws-eks-cluster/versions.tf
+++ terraform/aws-eks-cluster/versions.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = ">= 1.9.8"  # Specify your required version here
+  required_version = ">= 1.5.1"
 
-  # Optional: You can also specify required providers and their versions
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "5.75.1"
     }
   }
 }
