4b83dfabe454bb23c896a8ecabdeebe93e96a7b9
Shahar Shitrit <shahars86@gmail.com>
2024-11-14 16:07:39 +0200

    formatting

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,11 +1,11 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "20.29.0"
 
-  cluster_name = var.cluster_name
+  cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
-  vpc_id = var.vpc_id
-  subnet_ids = var.subnet_ids
+  vpc_id          = var.vpc_id
+  subnet_ids      = var.subnet_ids
 
   eks_managed_node_groups = {
     eks_nodes = {
